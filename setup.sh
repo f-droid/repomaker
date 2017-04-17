a5256aef53578f373bfaaeb4ffd66d5fcb9cd170
Torsten Grote <t@grobox.de>
2017-04-17 12:24:19 -0300

    Fix issue with background_task migration
    Split out development requirements

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
+set -x
 pip3 install -r requirements.txt --user && \
-python3 manage.py makemigrations background_task && \
 python3 manage.py makemigrations maker && \
 python3 manage.py migrate && \
 echo "All set up, now execute run.sh"
