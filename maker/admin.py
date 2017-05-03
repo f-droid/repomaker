1ff0206e6455ccbd094455838e11d43371491f4d
Torsten Grote <t@grobox.de>
2017-05-03 09:48:01 -0300

    Change order of repository and remote repository in the code

--- maker/admin.py
+++ maker/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from hvad.admin import TranslatableAdmin
 
-from .models import RemoteRepository, Repository, App, RemoteApp, Apk, ApkPointer, \
+from .models import Repository, RemoteRepository, App, RemoteApp, Apk, ApkPointer, \
     RemoteApkPointer, Category, Screenshot, RemoteScreenshot
 from .models.storage import StorageManager
 
-admin.site.register(RemoteRepository)
 admin.site.register(Repository)
+admin.site.register(RemoteRepository)
 admin.site.register(App, TranslatableAdmin)  # FIXME hides untranslated apps
 admin.site.register(RemoteApp, TranslatableAdmin)  # FIXME hides untranslated remote apps
 admin.site.register(Apk)
--- maker/models/repository.py
+++ maker/models/repository.py
@@ -72,146 +72,6 @@ class AbstractRepository(models.Model):
         return config
 
 
-class RemoteRepository(AbstractRepository):
-    users = models.ManyToManyField(User)
-    pre_installed = models.BooleanField(default=False)
-    index_etag = models.CharField(max_length=128, blank=True, null=True)
-    last_change_date = models.DateTimeField()
-    # TODO save mirrors from index
-
-    def get_path(self):
-        return os.path.join(settings.MEDIA_ROOT, get_remote_repo_path(self))
-
-    def update_async(self):
-        """
-        Schedules the repository to be updated asynchronously from remote location
-        """
-        if self.update_scheduled:
-            return  # no need to update a repo twice with same data
-        self.update_scheduled = True
-        self.save()
-        tasks.update_remote_repo(self.id)
-
-    def update_index(self, update_apps=True):
-        """
-        Downloads the remote index and passes it to update()
-
-        :raises: VerificationException() if the index can not be validated anymore
-        """
-        self.get_config()
-        repo_index, etag = index.download_repo_index(self.get_fingerprint_url(),
-                                                     etag=self.index_etag)
-        if repo_index is None:
-            return  # the index did not change since last time
-        self.index_etag = etag
-        self._update(repo_index, update_apps)  # also saves at the end
-
-    def _update(self, repo_index, update_apps):
-        """
-        Updates this remote repository with the given index
-
-        :param repo_index: The repository index v1 in JSON format
-        :param update_apps: False if apps should not be updated as well
-        """
-        # bail out if the repo did not change since last update
-        repo_change = datetime.datetime.fromtimestamp(repo_index['repo']['timestamp'] / 1000,
-                                                      timezone.utc)
-        if self.last_change_date and self.last_change_date >= repo_change:
-            return
-
-        # update repository's metadata
-        self.name = repo_index['repo']['name']
-        self.description = repo_index['repo']['description']
-        if update_apps:
-            self.last_change_date = repo_change
-        else:
-            # apps will be updated asynchronously soon, so this allows the update to pass
-            self.last_change_date = datetime.datetime.fromtimestamp(0, timezone.utc)
-        if not self.public_key:
-            self.public_key = repo_index['repo']['pubkey']  # added by index.download_repo_index()
-
-        # download and save repository icon
-        icon_url = self.url + '/' + repo_index['repo']['icon']
-        try:
-            # TODO use eTag like in RemoteApp._update_icon()
-            r = requests.get(icon_url)
-            if r.status_code != requests.codes.ok:
-                r.raise_for_status()
-            self.save()  # to ensure the primary key exists, to be used for the file path
-            self.delete_old_icon()
-            self.icon.save(repo_index['repo']['icon'], BytesIO(r.content), save=False)
-        except Exception as e:
-            logging.warning("Could not download repository icon from %s. %s", icon_url, e)
-
-        self.save()
-
-        if update_apps:
-            self._update_apps(repo_index['apps'], repo_index['packages'])
-
-    def _update_apps(self, apps, packages):
-        from maker.models.app import RemoteApp
-        # update the apps from this repo and remember all package names we have seen
-        package_names = []
-        for app in apps:
-            if app['packageName'] not in packages:
-                logging.info("App %s has no packages, so ignore it.", app['packageName'])
-                continue
-
-            # query for existing remote apps, if this repo was already saved
-            if self.pk:
-                query_set = RemoteApp.objects.filter(repo=self, package_id=app['packageName'])
-
-            # update existing app or create a new one
-            if self.pk and query_set.exists():
-                app_obj = query_set.get()
-            else:
-                app_obj = RemoteApp(package_id=app['packageName'], repo=self)
-            package_names.append(app['packageName'])
-            changed = app_obj.update_from_json(app)  # this also saves the app_obj
-
-            if changed:
-                # update packages belonging to app
-                for package in packages[app['packageName']]:
-                    self._update_package(app_obj, package)
-
-        # remove apps that no longer exist
-        self._remove_old_apps(package_names)
-
-    def _update_package(self, app, package_info):
-        from maker.models import Apk, RemoteApkPointer
-
-        apks = Apk.objects.filter(package_id=package_info['packageName'], hash=package_info['hash'])
-        if apks.exists():
-            apk = apks.get()
-        else:
-            apk = Apk.from_json(package_info)
-            apk.save()
-
-        pointers = RemoteApkPointer.objects.filter(apk=apk, app=app)
-        if not pointers.exists():
-            pointer = RemoteApkPointer(
-                apk=apk,
-                app=app,
-                url=self.url + "/" + package_info['apkName'],
-            )
-            pointer.save()
-
-    def _remove_old_apps(self, packages):
-        """
-        Removes old apps from the database and this repository.
-
-        :param packages: A list of package names that should not be removed
-        """
-        from maker.models.app import RemoteApp
-        old_apps = RemoteApp.objects.filter(repo=self).exclude(package_id__in=packages)
-        if old_apps.exists():
-            for app in old_apps.all():
-                app.delete()
-
-    class Meta(AbstractRepository.Meta):
-        verbose_name_plural = "Remote Repositories"
-
-
 class Repository(AbstractRepository):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     qrcode = models.ImageField(upload_to=get_repo_file_path, blank=True)
@@ -403,6 +263,146 @@ class Repository(AbstractRepository):
 
     class Meta(AbstractRepository.Meta):
         verbose_name_plural = "Repositories"
+
+
+class RemoteRepository(AbstractRepository):
+    users = models.ManyToManyField(User)
+    pre_installed = models.BooleanField(default=False)
+    index_etag = models.CharField(max_length=128, blank=True, null=True)
+    last_change_date = models.DateTimeField()
+    # TODO save mirrors from index
+
+    def get_path(self):
+        return os.path.join(settings.MEDIA_ROOT, get_remote_repo_path(self))
+
+    def update_async(self):
+        """
+        Schedules the repository to be updated asynchronously from remote location
+        """
+        if self.update_scheduled:
+            return  # no need to update a repo twice with same data
+        self.update_scheduled = True
+        self.save()
+        tasks.update_remote_repo(self.id)
+
+    def update_index(self, update_apps=True):
+        """
+        Downloads the remote index and passes it to update()
+
+        :raises: VerificationException() if the index can not be validated anymore
+        """
+        self.get_config()
+        repo_index, etag = index.download_repo_index(self.get_fingerprint_url(),
+                                                     etag=self.index_etag)
+        if repo_index is None:
+            return  # the index did not change since last time
+        self.index_etag = etag
+        self._update(repo_index, update_apps)  # also saves at the end
+
+    def _update(self, repo_index, update_apps):
+        """
+        Updates this remote repository with the given index
+
+        :param repo_index: The repository index v1 in JSON format
+        :param update_apps: False if apps should not be updated as well
+        """
+        # bail out if the repo did not change since last update
+        repo_change = datetime.datetime.fromtimestamp(repo_index['repo']['timestamp'] / 1000,
+                                                      timezone.utc)
+        if self.last_change_date and self.last_change_date >= repo_change:
+            return
+
+        # update repository's metadata
+        self.name = repo_index['repo']['name']
+        self.description = repo_index['repo']['description']
+        if update_apps:
+            self.last_change_date = repo_change
+        else:
+            # apps will be updated asynchronously soon, so this allows the update to pass
+            self.last_change_date = datetime.datetime.fromtimestamp(0, timezone.utc)
+        if not self.public_key:
+            self.public_key = repo_index['repo']['pubkey']  # added by index.download_repo_index()
+
+        # download and save repository icon
+        icon_url = self.url + '/' + repo_index['repo']['icon']
+        try:
+            # TODO use eTag like in RemoteApp._update_icon()
+            r = requests.get(icon_url)
+            if r.status_code != requests.codes.ok:
+                r.raise_for_status()
+            self.save()  # to ensure the primary key exists, to be used for the file path
+            self.delete_old_icon()
+            self.icon.save(repo_index['repo']['icon'], BytesIO(r.content), save=False)
+        except Exception as e:
+            logging.warning("Could not download repository icon from %s. %s", icon_url, e)
+
+        self.save()
+
+        if update_apps:
+            self._update_apps(repo_index['apps'], repo_index['packages'])
+
+    def _update_apps(self, apps, packages):
+        from maker.models.app import RemoteApp
+        # update the apps from this repo and remember all package names we have seen
+        package_names = []
+        for app in apps:
+            if app['packageName'] not in packages:
+                logging.info("App %s has no packages, so ignore it.", app['packageName'])
+                continue
+
+            # query for existing remote apps, if this repo was already saved
+            if self.pk:
+                query_set = RemoteApp.objects.filter(repo=self, package_id=app['packageName'])
+
+            # update existing app or create a new one
+            if self.pk and query_set.exists():
+                app_obj = query_set.get()
+            else:
+                app_obj = RemoteApp(package_id=app['packageName'], repo=self)
+            package_names.append(app['packageName'])
+            changed = app_obj.update_from_json(app)  # this also saves the app_obj
+
+            if changed:
+                # update packages belonging to app
+                for package in packages[app['packageName']]:
+                    self._update_package(app_obj, package)
+
+        # remove apps that no longer exist
+        self._remove_old_apps(package_names)
+
+    def _update_package(self, app, package_info):
+        from maker.models import Apk, RemoteApkPointer
+
+        apks = Apk.objects.filter(package_id=package_info['packageName'], hash=package_info['hash'])
+        if apks.exists():
+            apk = apks.get()
+        else:
+            apk = Apk.from_json(package_info)
+            apk.save()
+
+        pointers = RemoteApkPointer.objects.filter(apk=apk, app=app)
+        if not pointers.exists():
+            pointer = RemoteApkPointer(
+                apk=apk,
+                app=app,
+                url=self.url + "/" + package_info['apkName'],
+            )
+            pointer.save()
+
+    def _remove_old_apps(self, packages):
+        """
+        Removes old apps from the database and this repository.
+
+        :param packages: A list of package names that should not be removed
+        """
+        from maker.models.app import RemoteApp
+        old_apps = RemoteApp.objects.filter(repo=self).exclude(package_id__in=packages)
+        if old_apps.exists():
+            for app in old_apps.all():
+                app.delete()
+
+    class Meta(AbstractRepository.Meta):
+        verbose_name_plural = "Remote Repositories"
 
 
 @receiver(user_signed_up)
