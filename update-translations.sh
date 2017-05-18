c92759be247e4da9907720cb1ce7485ce5284977
Torsten Grote <t@grobox.de>
2017-05-18 09:44:34 -0300

    Get ready for translations on weblate
    Closes #46

--- update-translations.sh
+++ update-translations.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-python3 manage.py makemessages -a
+python3 manage.py makemessages --keep-pot --no-wrap --no-obsolete --ignore node_modules -v 3
 python3 manage.py compilemessages
