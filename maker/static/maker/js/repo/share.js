3e933cfc42b3ad1730324ed55bcc75b37db67394
Nico Alt <nicoalt@posteo.org>
2017-05-30 20:30:32 +0200

    Implement UI Design for Repo Index - Share View
    Fixes #74.

---
+++ maker/static/maker/js/repo/share.js
@@ -0,0 +1,22 @@
+var button = document.getElementsByClassName('rm-repo-share-share-copy')[0];
+button.style.display = 'inline-block';
+function copyLink(link) {
+    var textArea = document.createElement("textarea");
+    textArea.value = link;
+    document.body.appendChild(textArea);
+    textArea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copying link command was ' + msg);
+        buttonSetSuccessful();
+    } catch (err) {
+        console.log('Copying link command was ' + msg + ': ' + err);
+    }
+}
+
+function buttonSetSuccessful() {
+    button.className = 'rm-repo-share-share-copy--successful'
+    button.innerHTML = '<i class="material-icons">done</i>'
+}
