307d72e2c227f5ea85a2ef25dc10f2836faeb75c
Torsten Grote <t@grobox.de>
2017-06-06 17:18:21 -0300

    Remove empty <a> tags from app descriptions
    These empty links were caused by our HTML sanitizer
    removing links with unsupported protocols,
    but then leaving the empty link behind.
    
    An additional filter that is run after sanitation fixes this.
    
    Upstream ticket: https://github.com/mozilla/bleach/issues/288
    
    Fixes #60

--- maker/utils.py
+++ maker/utils.py
@@ -1,8 +1,31 @@
 import bleach
+from bleach.sanitizer import Cleaner
+from html5lib.filters.base import Filter
 
 
-ALLOWED_TAGS = bleach.sanitizer.ALLOWED_TAGS + ['p']
+class EmptyLinkFilter(Filter):
+    def __iter__(self):
+        remove_end_tag = False
+        for token in Filter.__iter__(self):
+            # only check anchor tags
+            if 'name' in token and token['name'] == 'a' and token['type'] in ['StartTag', 'EndTag']:
+                if token['type'] == 'StartTag' and token['data'] == {}:
+                    remove_end_tag = True
+                    continue
+                elif token['type'] == 'EndTag' and remove_end_tag:
+                    remove_end_tag = False
+                    continue
+            yield token
 
 
 def clean(text):
-    return bleach.clean(text, tags=ALLOWED_TAGS, strip=True)
+    cleaner = Cleaner(
+        tags=bleach.sanitizer.ALLOWED_TAGS + ['p', 'br'],
+        attributes=bleach.sanitizer.ALLOWED_ATTRIBUTES,
+        styles=bleach.sanitizer.ALLOWED_STYLES,
+        protocols=bleach.sanitizer.ALLOWED_PROTOCOLS,
+        strip=True,
+        strip_comments=True,
+        filters=[EmptyLinkFilter],
+    )
+    return cleaner.clean(text)
