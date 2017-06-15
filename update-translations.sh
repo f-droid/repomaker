0c9d37ffb828b6512cb090fdee76a3e8b62687a3
Ajeje Brazorf <lmelonimamo@yahoo.it>
2017-06-15 20:53:30 +0000

    Added translations from Weblate
    Also remove the line numbers from the .po files in the hope to reduce
    merge conflicts with weblate

--- update-translations.sh
+++ update-translations.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python3 manage.py makemessages --keep-pot --no-wrap --no-obsolete --ignore node_modules -v 3
-python3 manage.py makemessages --keep-pot --no-wrap --no-obsolete --ignore node_modules -v 3 -d djangojs
+python3 manage.py makemessages --keep-pot --no-wrap --no-location --no-obsolete --ignore node_modules -v 3
+python3 manage.py makemessages --keep-pot --no-wrap --no-location --no-obsolete --ignore node_modules -v 3 -d djangojs
 python3 manage.py compilemessages
