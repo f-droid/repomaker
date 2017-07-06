e4b5e87d7bc37260e3d6b1d50c067b59ae133472
ezjerry liao <ezjerry@gmail.com>
2017-07-06 06:48:51 +0000

    Update translations from Weblate

--- update-translations.sh
+++ update-translations.sh
@@ -3,3 +3,7 @@
 python3 manage.py makemessages --keep-pot --no-wrap --no-location --no-obsolete --ignore node_modules -v 3
 python3 manage.py makemessages --keep-pot --no-wrap --no-location --no-obsolete --ignore node_modules -v 3 -d djangojs
 python3 manage.py compilemessages
+
+sed -i -e '/^"POT-Creation-Date: /d' locale/*/LC_MESSAGES/django.po
+sed -i -e '/^"POT-Creation-Date: /d' locale/*/LC_MESSAGES/djangojs.po
+
