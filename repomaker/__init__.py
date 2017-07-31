c88e18d722fbcc5692010a2e4d672657994a13a4
Torsten Grote <t@grobox.de>
2017-07-31 18:02:53 -0300

    Add System Checks for requirements
    Closes #8

--- repomaker/__init__.py
+++ repomaker/__init__.py
@@ -1,6 +1,10 @@
 import os
 import sys
 
+from django.core.checks import Error, register
+from fdroidserver import common
+from fdroidserver.exception import FDroidException
+
 # The name of the default user. Please DO NOT CHANGE
 DEFAULT_USER_NAME = 'user'
 
@@ -43,3 +47,40 @@ def execute(params):
 
     # execute pending command
     execute_from_command_line(params)
+
+
+@register()
+def requirements_check(app_configs, **kwargs):  # pylint: disable=unused-argument
+    errors = []
+    config = {}
+    common.fill_config_defaults(config)
+    common.config = config
+    if 'keytool' not in config:
+        errors.append(
+            Error(
+                'Could not find `keytool` program.',
+                hint='This program usually comes with Java. Try to install JRE. '
+                     'On Debian-based system you can try to run '
+                     '`apt install openjdk-8-jre-headless`.',
+            )
+        )
+    if 'jarsigner' not in config and not common.set_command_in_config('apksigner'):
+        errors.append(
+            Error(
+                'Could not find `jarsigner` or `apksigner`. At least one of them is required.',
+                hint='Please install the missing tool. On Debian-based systems you can try to run '
+                     '`apt install apksigner`.',
+            )
+        )
+    try:
+        common.SdkToolsPopen(['aapt', 'version'], output=False)
+    except FDroidException:
+        errors.append(
+            Error(
+                'Could not find `aapt` program.',
+                hint='This program can be found in the Android SDK. '
+                     'On Debian-based systems you can also try to run `apt install aapt` '
+                     'to install it.',
+            )
+        )
+    return errors
