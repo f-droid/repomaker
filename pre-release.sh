85cd8ede77ecb91916593aaf9ea4acdc3ce47b8f
Nico Alt <nicoalt@posteo.org>
2018-11-06 14:05:01 +0100

    CI: Fix deploy task
    In pre-release, all directories except the former source directory get
    deleted. Changed this to the current source directory _src_ and also
    delete all other files there except the needed one.

--- pre-release.sh
+++ pre-release.sh
@@ -16,7 +16,8 @@ npm install
 rm -rf ${NODE}/dialog-polyfill/!(dialog-polyfill.*)
 
 # material-design-icons-iconfont remove unused files
-rm -rf ${NODE}/material-design-icons-iconfont/!(dist)
+rm -rf ${NODE}/material-design-icons-iconfont/!(src)
+rm -rf ${NODE}/material-design-icons-iconfont/src/!(material-design-icons.scss)
 rm -rf ${NODE}/material-design-icons-iconfont/.idea
 
 # material-design-lite remove unused files
