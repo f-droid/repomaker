1f698e935e5bc1fcd39ea5bdc17c60e19cc2ece4
Hans-Christoph Steiner <hans@eds.org>
2020-09-16 18:07:33 +0200

    support running tests with Debian's coverage, or other installs

--- tests/test-units.sh
+++ tests/test-units.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-coverage3 run --source='repomaker' --omit="*/wsgi.py","repomaker/__init__.py" manage.py test --settings repomaker.settings_test &&
-coverage3 run --append --source='repomaker' --omit="*/wsgi.py","repomaker/__init__.py" manage.py test --settings repomaker.settings_test_multi_user &&
-coverage3 report
+coverage=coverage
+if which python3-coverage; then
+    coverage=python3-coverage
+elif which coverage3; then
+    coverage=coverage3
+fi
+
+$coverage run --source='repomaker' --omit="*/wsgi.py","repomaker/__init__.py" manage.py test --settings repomaker.settings_test &&
+$coverage run --append --source='repomaker' --omit="*/wsgi.py","repomaker/__init__.py" manage.py test --settings repomaker.settings_test_multi_user &&
+$coverage report
